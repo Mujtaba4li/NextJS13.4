5b4c25688fbba78f87ce39f9f95e98a05808653e
Mujtaba Shafique <mujtaba.shafique@outlook.com>
2023-10-25 15:49:28 +0500

    Initialize prima

--- src/app/(dashboard)/todos/page.tsx
+++ src/app/(dashboard)/todos/page.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import TodoList from '@/components/TodoList'
+import db from '@/utils/db'
+import { todo } from 'node:test'
 
-const todos = () => {
-	return <div>todos</div>
+import React from 'react'
+const getData=async () =>{
+	const todos=await db.todo.findMany({})
+	return todo
+}
+const todos = async() => {
+	const todo=await getData();
+	console.log(todo);
+	return <div>
+		<TodoList todo={todo}/>
+	</div>
 }
 
 export default todos
